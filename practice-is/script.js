1a63f108c08b548cc112c71018c3d9471ec06185
rabia-aaziz <39001798+rabia-aaziz@users.noreply.github.com>
2020-12-21 07:33:21 +0500

    Add files via upload

---
+++ practice-is/script.js
@@ -0,0 +1,64 @@
+
+// function showAlert(){
+//    const numb = document.querySelector('input').value;
+// console.log(numb*1);
+// console.log(numb*2);
+// console.log(numb*3);
+// console.log(numb*4);
+// console.log(numb*5);
+// console.log(numb*6);
+// console.log(numb*7);
+// console.log(numb*8);
+// console.log(numb*9);
+// console.log(numb*10);
+
+// }
+
+// function showAlert() {
+//   let inputElement = document.querySelector('#fname');
+//   console.log(inputElement);
+
+//   let inputValue = inputElement.value;
+//   console.log(inputValue);
+
+//   let headingELement = document.querySelector('span');
+//   console.log(headingELement);
+//   document.querySelector('span').innerHTML = inputValue;
+
+//   let inputElement2 = document.querySelector('#lname');
+//   console.log(inputElement2);
+
+//   let inputValue2 = inputElement2.value;
+//   console.log(inputValue2);
+
+//   let lastName = document.querySelector('.name');
+//   console.log(lastName);
+//   document.querySelector('.name').innerHTML = inputValue2;
+
+// }
+
+
+function light(){
+// const costOfApples= parseInt(document.querySelector('input').value);
+// alert(costOfApples);
+// console.log(typeof costOfApples);
+document.body.style.backgroundColor = "yellow";
+document.querySelector("h1").innerHTML = "Light";
+}
+function dark(){
+document.body.style.backgroundColor = "black";
+document.querySelector("h1").innerHTML = "Dark";
+document.querySelector("h1").style.color = "white";
+
+
+
+}
+
+
+
+
+
+
+
+
+
